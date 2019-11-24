c93ffd6546d75c2e7e97716e8e0fe0248ff1e418
alanmaranto <alanarostegui@hotmail.com>
2019-11-23 21:35:55 -0600

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>Started</div>
+     );
+  }
+}
+ 
+export default App;
