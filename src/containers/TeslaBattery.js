0831ae69410588c61fe8a7b2cc14c271f7e67f9b
alanmaranto <alanarostegui@hotmail.com>
2019-11-30 23:33:35 -0600

    Tesla Counter

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -5,6 +5,7 @@ import "./TeslaBattery.css";
 import TeslaNotice from "../components/TeslaNotice/TeslaNotice";
 import TeslaCar from "../components/TeslaCar/TeslaCar";
 import TeslaStats from '../components/TeslaStats/TeslaStats';
+import TeslaCounter from '../components/TeslaCounter/TeslaCounter';
 
 const config = {
   speed: 55,
@@ -28,6 +29,53 @@ class TeslaBattery extends Component {
       this.statsUpdate();
   }
 
+  updateCounterState = (title, newValue) => {
+      const { config } = this.state;
+      title === 'Speed' ? config['speed'] = newValue : config['temperature'] = newValue
+    //   title === 'Speed' ? config.speed = newValue : config.temperature = newValue
+    this.setState({ config });
+  }
+
+  increment = (e, title) => {
+      e.preventDefault();
+      let currentValue, maxValue, step;
+      const { config } = this.state;
+      const { speed, temperature } = this.props.counterDefaultVal;
+      if (title === 'Speed') {
+          currentValue = config.speed;
+          maxValue = speed.max;
+          step = speed.step;
+      } else {
+          currentValue = config.temperature;
+          maxValue = temperature.max;
+          step = temperature.step;
+      }
+      if (currentValue < maxValue ) {
+          const newValue = currentValue + step;
+          this.updateCounterState(title, newValue)
+      }
+  }
+
+  decrement = (e, title) => {
+      e.preventDefault();
+      let currentValue, minValue, step;
+      const { config } = this.state;
+      const { speed, temperature } = this.props.counterDefaultVal;
+      if (title === 'Speed') {
+          currentValue = config.speed;
+          minValue = speed.min;
+          step = speed.step;
+      } else {
+          currentValue = config.temperature;
+          minValue = temperature.min;
+          step = temperature.step;
+      }
+      if (currentValue > minValue) {
+          const newValue = currentValue - step;
+          this.updateCounterState(title, newValue)
+      }
+  } 
+
   calculateStats = (models, value) => {
       const dataModels = getModelData();
       return models.map(model => {
@@ -50,11 +98,20 @@ class TeslaBattery extends Component {
 
   render() {
     const { config, carstats } = this.state;
+    const { counterDefaultVal } = this.props;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
         <TeslaCar wheelsize={config.wheels} />
         <TeslaStats carstats={carstats} />
+        <div>
+            <TeslaCounter 
+                currentValue={config.speed}
+                initValues={counterDefaultVal.speed}
+                increment={this.increment}
+                decrement={this.decrement}
+            />
+        </div>
         <TeslaNotice />
       </form>
     );
