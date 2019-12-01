bc2f005eeaa698ba07a6fb35e475554dda9e995e
alanmaranto <alanarostegui@hotmail.com>
2019-11-30 20:59:39 -0600

    bugfix

--- src/components/TeslaCar/TeslaCar.js
+++ src/components/TeslaCar/TeslaCar.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./TeslaCar.css";
 import PropTypes from 'prop-types';
 
-const TeslaCar = props => (
+const TeslaCar = props => {
+  return (
   <div className="tesla-car">
     <div className="tesla-wheels">
       <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
-      <div className={`tesla-wheel tesla-wheel--rear tesña-wheel--${props.wheelsize}`}></div>
+      <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
     </div>
   </div>
-);
+  )}
+;
 TeslaCar.propTypes = {
     wheelsize: PropTypes.number
 }
