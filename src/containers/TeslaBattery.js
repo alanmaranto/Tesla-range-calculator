cf988dcdc874f4fd5bed428654fb0d5d9e98f478
alanmaranto <alanarostegui@hotmail.com>
2019-11-30 20:28:42 -0600

    Passing props to TeslaCar and state of application

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react'
-import './TeslaBattery.css';
-import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
-import TeslaCar from '../components/TeslaCar/TeslaCar';
+import React, { Component } from "react";
+import "./TeslaBattery.css";
+import TeslaNotice from "../components/TeslaNotice/TeslaNotice";
+import TeslaCar from "../components/TeslaCar/TeslaCar";
+
+const config = {
+  speed: 55,
+  temperature: 20,
+  climate: true,
+  wheels: 19
+};
+
+const initialState = {
+  config,
+  carstats: []
+};
 
 class TeslaBattery extends Component {
-    render() { 
-        return ( 
-            <form className="tesla-battery">
-                <h1>Range Per Charge</h1>
-                <TeslaCar />
-                <TeslaNotice />
-            </form>
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+  render() {
+    const { config } = this.state;
+    return (
+      <form className="tesla-battery">
+        <h1>Range Per Charge</h1>
+        <TeslaCar wheelsize={config.wheels}/>
+        <TeslaNotice />
+      </form>
+    );
+  }
 }
- 
+
 export default TeslaBattery;
