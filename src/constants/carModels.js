7eff8f881f55e98621aca5176fe32cd04934c5ba
alanmaranto <alanarostegui@hotmail.com>
2019-11-30 22:19:15 -0600

    CalculateStats and setState

---
+++ src/constants/carModels.js
@@ -0,0 +1,10 @@
+export const carModels = [
+    '60', 
+    '60D', 
+    '75', 
+    '75D', 
+    '90D', 
+    'P100D'
+];
+
+export default carModels;
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { getModelData } from '../services/BatteryService';
+import { carModels } from '../constants/carModels';
 import "./TeslaBattery.css";
 import TeslaNotice from "../components/TeslaNotice/TeslaNotice";
 import TeslaCar from "../components/TeslaCar/TeslaCar";
@@ -21,6 +23,31 @@ class TeslaBattery extends Component {
     super(props);
     this.state = initialState;
   }
+
+  componentDidMount() {
+      this.statsUpdate();
+  }
+
+  calculateStats = (models, value) => {
+      const dataModels = getModelData();
+      return models.map(model => {
+          const { speed, temperature, climate, wheels } = value;
+          const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+          return {
+              model,
+              miles
+          };
+      });
+  }
+
+  statsUpdate = () => {
+      const { config } = this.state;
+    this.setState({
+        carstats: this.calculateStats(carModels, config)
+    })
+  }
+
+
   render() {
     const { config, carstats } = this.state;
     return (
